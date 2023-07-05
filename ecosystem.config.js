27db58c66320794045a505cc4b468b59a22d7b77
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2023-07-05 07:05:00 +0300

    path fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: "./.env.deploy"});
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
@@ -7,7 +7,7 @@ const {
 module.exports = {
   apps : [{
     name   : "app1",
-    script : "./app.js"
+    script : "./dist/app.js"
   }],
 
   deploy: {
@@ -15,7 +15,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'git@github.com:Qolit3/Deploy-back.git',
+      repo: 'https://github.com/Qolit3/Deploy-back.git',
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} &&  scp ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
